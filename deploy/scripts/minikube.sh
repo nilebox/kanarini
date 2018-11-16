2b1c8b9d0c034d0bea3aabf12d3b5f358bb99a74
Nail Islamov <nislamov@atlassian.com>
2018-11-16 23:19:14 +1100

    Examples cleanup

--- deploy/scripts/minikube.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# set -euo pipefail
-# cd "$( dirname "${BASH_SOURCE[0]}" )"
-# set -o xtrace
-
-minikube delete
-minikube start --kubernetes-version=v1.10.1 --memory=4096 --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.address=0.0.0.0 --extra-config=controller-manager.address=0.0.0.0
